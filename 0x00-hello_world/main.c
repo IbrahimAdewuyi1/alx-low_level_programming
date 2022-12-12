7d0991a97a0e8b6bc1548bc06d16c1d653b1ca8c
Adewuyi Ibrahim <ibrahimadewuyiadebayo@gmail.com>
2022-12-12 14:01:19 -0800

    fix error 3

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-int main(void) {
-	return(0);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Holberton School");
+	return (0);
 }
 
